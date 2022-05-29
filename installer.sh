a82a111ff65b463cd59e826c8d7782847e793b55
Isaque Monteiro <isaque.monteiror@gmail.com>
2022-05-29 02:24:31 -0300

    inicio instalador

---
+++ installer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+clear
+
+java -version
+
+if [$? -eq 0]
+then
+else
+        sudo add-apt-repository ppa:webupd8team/java -y
+        clear
+        sudo apt update -y
+        clear
+        sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
+        clear
+fi
+
+clear
+
+docker --version
+
+if [$? -eq 0]
+then
+else
+        sudo apt-get docker.io
+        clear
+        sudo docker pull mysql:5.7
+        clear
+fi
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+cd javaDocker
+
+cd database
+
+sudo docker build -t ingresse/local .
+
+sudo docker run -p3306:3306 -d --name ingressedb ingresse/local
+
+sudo apt-get install mysql-client -y
+
+sudo docker exec -it ingressedb /bin/bash
+
+mysql -uingresse -purubu100
+
+SHOW DATABASES;
+
+USE logs-ingresse;
+
+SHOW TABLES;
+
+DESC logs;
+
+
